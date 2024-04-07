3e38939c91ddbb5a00c8b4cf42f87ba3f0f1b1f3
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-04-07 16:44:19 -0700

    Opens a tmux session and pipes output to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{any::Any, io::{stdin, stdout, Read, Write}, process::{self, exit, Command}, u8, usize};
+use std::{any::Any, io::{self, stdin, stdout, Read, Write}, process::{self, exit, Command, Stdio}, u8, usize};
 
 use colored::Colorize;
 use console::Term;
+use crossterm::{execute, terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
 
 
 
@@ -14,6 +15,7 @@ fn main() {
 
 
 
+    let mut capture_output = true;
     let term = Term::stdout();
 
     let mut input = String::new();
@@ -22,7 +24,7 @@ fn main() {
     let mut filtered_arr = filter(&files, &input);
     let mut highlighted_index = filtered_arr.len()-1;
 
-    loop {
+    while capture_output {
 
         term.clear_screen();
 
@@ -47,40 +49,47 @@ fn main() {
         let mut cur = 0;
 
 
-        let key = term.read_key().unwrap();
-        match key {
-            console::Key::Char(c) => {
-                input = input + &c.to_string(); 
-                filtered_arr = filter(&files, &input);
-                highlighted_index = filtered_arr.len()-1;
-            } ,
-            console::Key::Backspace => {
-                if input.len() > 0 {
-                    input.truncate(input.len() - 1);
-                }
-                filtered_arr = filter(&files, &input);
-                highlighted_index = filtered_arr.len()-1;
-            },
-            console::Key::Enter => {
-                Command::new("tmux")
-                    .arg("new")
-                    .spawn()
-                    .unwrap();
-                    
-            },
-            console::Key::ArrowUp => {
-                highlighted_index -= 1;
-            },
-            console::Key::ArrowDown => {
-                highlighted_index += 1;
-            },
-            _ => (),
-        }
+            
+            let key = term.read_key().unwrap();
+            match key {
+                console::Key::Char(c) => {
+                    input = input + &c.to_string(); 
+                    filtered_arr = filter(&files, &input);
+                    highlighted_index = filtered_arr.len()-1;
+                } ,
+                console::Key::Backspace => {
+                    if input.len() > 0 {
+                        input.truncate(input.len() - 1);
+                    }
+                    filtered_arr = filter(&files, &input);
+                    highlighted_index = filtered_arr.len()-1;
+                },
+                console::Key::Enter => {
+                    capture_output = false;
+                    execute!(stdout(), EnterAlternateScreen).unwrap();
+                    let tmux_session = Command::new("tmux")
+                        .arg("new")
+                        .stdout(Stdio::piped())
+                        .stderr(Stdio::piped())
+                        .spawn()
+                        .unwrap()
+                        .wait();
 
+                    execute!(stdout(), LeaveAlternateScreen ).unwrap();
+
+                },
+                console::Key::ArrowUp => {
+                    highlighted_index -= 1;
+                },
+                console::Key::ArrowDown => {
+                    highlighted_index += 1;
+                },
+                _ => (),
+            }
 
     }
 
-
+    print!("\x1B[2J\x1B[1;1H");
 }
 
 
