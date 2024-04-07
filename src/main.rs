f8ed12515bb2932876903a97a5639091581838fc
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-04-07 16:10:10 -0700

    Display for projects is "smarter"

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::{any::Any, io::{stdin, stdout, Read, Write}, process::exit, u8};
+use std::{any::Any, io::{stdin, stdout, Read, Write}, process::{self, exit, Command}, u8, usize};
 
 use colored::Colorize;
 use console::Term;
-
-
 
 
 
@@ -25,20 +23,29 @@ fn main() {
     let mut highlighted_index = filtered_arr.len()-1;
 
     loop {
-        print!("{}[2J", 27 as char);
 
+        term.clear_screen();
+
+        let (row, _) = term.size();
+        
+        let entrySize = row - 1;
         filtered_arr = filter(&files, &input);
-        let mut cur = 0;
-        for file in &filtered_arr  {
-            if cur == highlighted_index {
-                println!("{}{}", " > ".green() ,&file.green());
-            } else {
-                println!(" > {}", &file);
-            }
-            cur += 1;
-        }
+        let mut idx = 0;
 
-        println!("\n ({}/{})\n\n > {} ", &filtered_arr.len(), &files.len(), &input);
+        while idx < entrySize {
+            let cursor = entrySize - idx;
+            if usize::from(cursor) > filtered_arr.len()   {
+                print!("\n");
+            } else if filtered_arr.len() > 0{
+                print!("> {} \n", filtered_arr.get(usize::from(cursor - 1)).unwrap());
+            }
+            idx += 1; 
+        }
+        print!("input: {} \n", input);
+
+
+        let mut cur = 0;
+
 
         let key = term.read_key().unwrap();
         match key {
@@ -55,7 +62,11 @@ fn main() {
                 highlighted_index = filtered_arr.len()-1;
             },
             console::Key::Enter => {
-                exit(1);
+                Command::new("tmux")
+                    .arg("new")
+                    .spawn()
+                    .unwrap();
+                    
             },
             console::Key::ArrowUp => {
                 highlighted_index -= 1;
