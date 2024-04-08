02c319aaa94c5deb4b69b9817a3b234285bf83e7
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-04-07 17:06:27 -0700

    Small tweaks

--- src/main.rs
+++ src/main.rs
@@ -17,76 +17,69 @@ fn main() {
 
     let mut capture_output = true;
     let term = Term::stdout();
-
+    
     let mut input = String::new();
 
 
     let mut filtered_arr = filter(&files, &input);
-    let mut highlighted_index = filtered_arr.len()-1;
 
     while capture_output {
 
-        term.clear_screen();
+        term.clear_screen().unwrap();
 
         let (row, _) = term.size();
         
-        let entrySize = row - 1;
+        let entry_size = row - 2;
         filtered_arr = filter(&files, &input);
         let mut idx = 0;
 
-        while idx < entrySize {
-            let cursor = entrySize - idx;
+        while idx < entry_size {
+            let cursor = entry_size - idx;
             if usize::from(cursor) > filtered_arr.len()   {
                 print!("\n");
             } else if filtered_arr.len() > 0{
-                print!("> {} \n", filtered_arr.get(usize::from(cursor - 1)).unwrap());
+                if cursor == 1 {
+                    print!("> {} \n", &filtered_arr.get(usize::from(cursor - 1)).unwrap().green());
+                } else {
+                    print!("  {} \n", &filtered_arr.get(usize::from(cursor - 1)).unwrap());
+                }
             }
             idx += 1; 
         }
+        print!("  {}/{} \n", &filtered_arr.len(), files.len());
         print!("input: {} \n", input);
 
 
-        let mut cur = 0;
 
+        let key = term.read_key().unwrap();
+        match key {
+            console::Key::Char(c) => {
+                input = input + &c.to_string(); 
+                filtered_arr = filter(&files, &input);
+            } ,
+            console::Key::Backspace => {
+                if input.len() > 0 {
+                    input.truncate(input.len() - 1);
+                }
+                filtered_arr = filter(&files, &input);
+            },
+            console::Key::Enter => {
+                capture_output = false;
+                execute!(stdout(), EnterAlternateScreen).unwrap();
+                let dir = filtered_arr.get(0).unwrap();
+                let tmux_session = Command::new("tmux")
+                    .args(["new", "-A", "-s", dir.as_str(), "-c", dir.as_str()])
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::piped())
+                    .spawn()
+                    .unwrap()
+                    .wait();
 
-            
-            let key = term.read_key().unwrap();
-            match key {
-                console::Key::Char(c) => {
-                    input = input + &c.to_string(); 
-                    filtered_arr = filter(&files, &input);
-                    highlighted_index = filtered_arr.len()-1;
-                } ,
-                console::Key::Backspace => {
-                    if input.len() > 0 {
-                        input.truncate(input.len() - 1);
-                    }
-                    filtered_arr = filter(&files, &input);
-                    highlighted_index = filtered_arr.len()-1;
-                },
-                console::Key::Enter => {
-                    capture_output = false;
-                    execute!(stdout(), EnterAlternateScreen).unwrap();
-                    let dir = filtered_arr.get(0).unwrap();
-                    let tmux_session = Command::new("tmux")
-                        .args(["new", "-A", "-s", dir.as_str(), "-c", dir.as_str()])
-                        .stdout(Stdio::piped())
-                        .stderr(Stdio::piped())
-                        .spawn()
-                        .unwrap()
-                        .wait();
+                execute!(stdout(), LeaveAlternateScreen ).unwrap();
 
-                    execute!(stdout(), LeaveAlternateScreen ).unwrap();
-
-                },
-                console::Key::ArrowUp => {
-                    highlighted_index -= 1;
-                },
-                console::Key::ArrowDown => {
-                    highlighted_index += 1;
-                },
-                _ => (),
-            }
+            },
+            _ => (),
+        }
 
     }
 
