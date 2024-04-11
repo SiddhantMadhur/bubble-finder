0315b947d7b08916b725b1750da6c72e4efe8ba0
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-04-11 15:09:32 -0700

    Excludes node_modules (need to find better way)

--- src/file_explorer.rs
+++ src/file_explorer.rs
@@ -7,7 +7,9 @@ use crate::config::Config;
 
 fn is_hidden(entry: &walkdir::DirEntry) -> bool {
     let file_name = entry.file_name().to_str().expect("Could not convert");
-    file_name.starts_with(".") && file_name != ".git"
+    let is_dot_file = file_name.starts_with(".") && file_name != ".git"; 
+    let is_node_module = file_name == "node_modules";
+    is_node_module || is_dot_file
 }
 
 
@@ -30,6 +32,7 @@ pub fn get_all_files(config: &Config) -> Vec<String> {
                             .expect("Could not convert")
                             .replace("/.git", "")
                             );
+
                     }
                 },
                 Err(_) => () 
